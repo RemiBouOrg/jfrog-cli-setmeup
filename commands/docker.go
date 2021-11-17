45630739e1b69b7f702105bc0ff7ab8373438994
Rémi Bourgarel <remib@jfrog.com>
2021-11-18 00:12:14 +0100

    add docker handler

--- commands/docker.go
+++ commands/docker.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/jfrog/jfrog-client-go/utils/log"
 	"github.com/pkg/errors"
+	"net/http"
 	"net/url"
 	"os/exec"
 	"strconv"
@@ -16,52 +17,9 @@ func handleDocker(configuration SetMeUpConfiguration) error {
 	if err != nil {
 		return err
 	}
-	var host string
-	var port string
-	parseArtiUrl, err := url.Parse(configuration.serverDetails.ArtifactoryUrl)
+	host, port, err := findDockerHostAndPort(configuration, get, jsonBytes)
 	if err != nil {
 		return err
-	}
-	if get.StatusCode == 403 {
-		// if 403 it's likely a cloud instance, we'll try to login with the hostname
-		// if 403 is due to bad creds it will fail anyway
-		host = parseArtiUrl.Hostname()
-		port = parseArtiUrl.Port()
-		log.Info("Artifactory is likely a cloud instance")
-	} else {
-		proxySetting := proxySettings{}
-		err := json.Unmarshal(jsonBytes, &proxySetting)
-		if err != nil {
-			return err
-		}
-		if proxySetting.UseHttps {
-			port = strconv.Itoa(proxySetting.HttpsPort)
-		} else {
-			port = strconv.Itoa(proxySetting.HttpPort)
-		}
-		switch proxySetting.DockerReverseProxyMethod {
-		case "SUBDOMAIN":
-			host = fmt.Sprintf("%s.%s", configuration.repositoryKey, parseArtiUrl.Hostname())
-			log.Info(fmt.Sprintf("Using subdomain per repository technique with %s:%s", host, port))
-		case "REPOPATHPREFIX":
-			host = parseArtiUrl.Hostname()
-			log.Info(fmt.Sprintf("Using path prefix technique on %s:%s", host, port))
-		case "PORTPERREPO":
-			for _, portConfig := range proxySetting.ReverseProxyRepositories.ReverseProxyRepoConfigs {
-				if portConfig.RepoRef == configuration.repositoryKey {
-					host = portConfig.ServerName
-					port = strconv.Itoa(portConfig.Port)
-					break
-				}
-			}
-			if host == "" {
-				return fmt.Errorf("unable to find port config for %s", configuration.repositoryKey)
-			}
-			log.Info(fmt.Sprintf("Using path prefix per repository technique with %s:%s", host, port))
-		default:
-			return fmt.Errorf("non handled method %s")
-
-		}
 	}
 	authConfig, _ := configuration.serverDetails.CreateArtAuthConfig()
 	command := exec.Command("docker",
@@ -79,21 +37,75 @@ func handleDocker(configuration SetMeUpConfiguration) error {
 	return nil
 }
 
-type reverseProxyRepoConfigs struct {
-	RepoRef    string
-	Port       int
-	ServerName string
-}
+func findDockerHostAndPort(configuration SetMeUpConfiguration, webServerResponse *http.Response, webServerJson []byte) (string, string, error) {
 
-type reverseProxyRepositories struct {
-	ReverseProxyRepoConfigs []reverseProxyRepoConfigs
+	if webServerResponse.StatusCode == 403 {
+		parseArtiUrl, err := url.Parse(configuration.serverDetails.ArtifactoryUrl)
+		if err != nil {
+			return "", "", err
+		}
+		// if 403 it's likely a cloud instance, we'll try to login with the hostname
+		// if 403 is due to bad creds it will fail anyway
+		log.Info("Artifactory is likely a cloud instance")
+		return parseArtiUrl.Hostname(), "443", nil
+	} else {
+		var port string
+		proxySetting := proxySettings{}
+		err := json.Unmarshal(webServerJson, &proxySetting)
+		if err != nil {
+			return "", "", err
+		}
+		if proxySetting.UseHttps {
+			port = strconv.Itoa(proxySetting.HttpsPort)
+		} else {
+			port = strconv.Itoa(proxySetting.HttpPort)
+		}
+		switch proxySetting.DockerReverseProxyMethod {
+		case "SUBDOMAIN":
+			host := fmt.Sprintf("%s.%s", configuration.repositoryKey, proxySetting.ServerName)
+			log.Info(fmt.Sprintf("Using subdomain per repository technique with %s:%s", host, port))
+			return host, port, nil
+		case "REPOPATHPREFIX":
+			host := proxySetting.ServerName
+			log.Info(fmt.Sprintf("Using path prefix technique on %s:%s", host, port))
+			return host, port, nil
+		case "PORTPERREPO":
+			var host string
+			for _, portConfig := range proxySetting.ReverseProxyRepositories.ReverseProxyRepoConfigs {
+				if portConfig.RepoRef == configuration.repositoryKey {
+					host = portConfig.ServerName
+					port = strconv.Itoa(portConfig.Port)
+					break
+				}
+			}
+			if host == "" {
+				return "", "", fmt.Errorf("unable to find port config for %s", configuration.repositoryKey)
+			}
+			log.Info(fmt.Sprintf("Using path prefix per repository technique with %s:%s", host, port))
+			return host, port, nil
+		default:
+			return "", "", fmt.Errorf("non handled method %s", proxySetting.DockerReverseProxyMethod)
+
+		}
+	}
 }
 
 type proxySettings struct {
+	ServerName               string
 	UseHttp                  bool
 	UseHttps                 bool
 	HttpPort                 int
 	HttpsPort                int
 	DockerReverseProxyMethod string
 	ReverseProxyRepositories reverseProxyRepositories
+}
+
+type reverseProxyRepositories struct {
+	ReverseProxyRepoConfigs []reverseProxyRepoConfigs
+}
+
+type reverseProxyRepoConfigs struct {
+	RepoRef    string
+	Port       int
+	ServerName string
 }
---
+++ commands/docker_test.go
@@ -0,0 +1,146 @@
+package commands
+
+import (
+	"encoding/json"
+	"github.com/jfrog/jfrog-cli-core/v2/utils/config"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"testing"
+)
+
+func Test_FindDockerHostPort(t *testing.T) {
+	testData := []struct {
+		name                string
+		webServerJson       proxySettings
+		webServerStatusCode int
+		repoKey             string
+		artiUrl             string
+		expectedHost        string
+		expectedPort        string
+		expectErr           bool
+	}{
+		{
+			name:                "On 403 use arti host and https",
+			webServerStatusCode: 403,
+			artiUrl:             "https://google.com",
+			expectedHost:        "google.com",
+			expectedPort:        "443",
+		},
+		{
+			name:         "On SUBDOMAIN concat repo key with hostname",
+			repoKey:      "test",
+			expectedHost: "test.google.com",
+			expectedPort: "443",
+			webServerJson: proxySettings{
+				ServerName:               "google.com",
+				DockerReverseProxyMethod: "SUBDOMAIN",
+				HttpsPort:                443,
+				UseHttps:                 true,
+			},
+		},
+		{
+			name:         "On SUBDOMAIN if no ttps use http port",
+			repoKey:      "test",
+			expectedHost: "test.google.com",
+			expectedPort: "444",
+			webServerJson: proxySettings{
+				ServerName:               "google.com",
+				DockerReverseProxyMethod: "SUBDOMAIN",
+				HttpsPort:                443,
+				HttpPort:                 444,
+				UseHttps:                 false,
+			},
+		},
+		{
+			name:         "On REPOPATHPREFIX use server name",
+			artiUrl:      "google.com",
+			repoKey:      "test",
+			expectedHost: "google.com",
+			expectedPort: "443",
+			webServerJson: proxySettings{
+				ServerName:               "google.com",
+				DockerReverseProxyMethod: "REPOPATHPREFIX",
+				HttpsPort:                443,
+				UseHttps:                 true,
+			},
+		},
+		{
+			name:         "On PORTPERREPO use specific port and host",
+			artiUrl:      "google.com",
+			repoKey:      "test",
+			expectedHost: "testgoogle.com",
+			expectedPort: "888",
+			webServerJson: proxySettings{
+				ServerName:               "google.com",
+				DockerReverseProxyMethod: "PORTPERREPO",
+				ReverseProxyRepositories: reverseProxyRepositories{
+					ReverseProxyRepoConfigs: []reverseProxyRepoConfigs{
+						{RepoRef: "test", Port: 888, ServerName: "testgoogle.com"},
+						{RepoRef: "test2", Port: 999, ServerName: "test3google.com"},
+					}},
+				HttpsPort: 443,
+				UseHttps:  true,
+			},
+		},
+		{
+			name:      "On PORTPERREPO if no port, then err",
+			artiUrl:   "google.com",
+			repoKey:   "test3",
+			expectErr: true,
+			webServerJson: proxySettings{
+				ServerName:               "google.com",
+				DockerReverseProxyMethod: "PORTPERREPO",
+				ReverseProxyRepositories: reverseProxyRepositories{
+					ReverseProxyRepoConfigs: []reverseProxyRepoConfigs{
+						{RepoRef: "test", Port: 888, ServerName: "testgoogle.com"},
+						{RepoRef: "test2", Port: 999, ServerName: "test3google.com"},
+					}},
+				HttpsPort: 443,
+				UseHttps:  true,
+			},
+		},
+		{
+			name:      "On unexpected method, err",
+			artiUrl:   "google.com",
+			repoKey:   "test3",
+			expectErr: true,
+			webServerJson: proxySettings{
+				ServerName:               "google.com",
+				DockerReverseProxyMethod: "DONTEXISTS",
+			},
+		},
+	}
+	for _, test := range testData {
+		t.Run(test.name, func(t *testing.T) {
+			webServerJsonStr, _ := json.Marshal(test.webServerJson)
+			host, port, err := findDockerHostAndPort(
+				SetMeUpConfiguration{
+					serverDetails: &config.ServerDetails{ArtifactoryUrl: test.artiUrl},
+					repositoryKey: test.repoKey,
+				},
+				&http.Response{StatusCode: test.webServerStatusCode},
+				webServerJsonStr)
+			if test.expectErr {
+				require.Error(t, err)
+			} else {
+				require.Equal(t, test.expectedPort, port)
+				require.Equal(t, test.expectedHost, host)
+			}
+		})
+	}
+}
+
+func Test_handleDocker(t *testing.T) {
+	testDockerRepo := getRepoListFromDefaultServer("docker")[0].Key
+	err := handleDocker(
+		SetMeUpConfiguration{
+			repositoryKey: testDockerRepo,
+			serverDetails: serverDetails,
+			repoDetails: RepoDetails{
+				PackageType: "docker",
+				Key:         testDockerRepo,
+			},
+		},
+	)
+	require.NoError(t, err)
+}
